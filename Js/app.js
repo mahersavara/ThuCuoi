49bc29996566abd82ce3cf3b08c71191940ef28a
mahersavara <kdlinh8811@gmail.com>
2022-11-23 06:03:08 +0700

    Deploy redirect to render.com

--- Js/app.js
+++ Js/app.js
@@ -1,11 +1,11 @@
-const index = require ("express")();
+const app = require ("express")();
 
 const PORT = process.env.PORT || 3000;
 
-index.get("",(req, res) => {
+app.get("",(req, res) => {
     res.send("Hello World");
 });
 
-index.listen(PORT,()=> {
+app.listen(PORT,()=> {
     console.log(`App up at port ${PORT}`);
 })
