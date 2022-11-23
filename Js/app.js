158ccecfee64cbc129a34199ec49bad11f2d7636
mahersavara <kdlinh8811@gmail.com>
2022-11-24 01:33:37 +0700

    Update scss dependencies + build html source

--- Js/app.js
+++ Js/app.js
@@ -1,11 +1,26 @@
-const app = require ("express")();
+// const app = require ("express")();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("",(req, res) => {
-    res.send("Hello Worlsas");
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+// app.get("",(req, res) => {
+//     res.send("Hello Worlsas");
+// });
+router.get('/',function(req,res){
+    res.sendFile('index.html', { root: "./Html/"});
+    //__dirname : It will resolve to your project folder.
 });
 
-app.listen(PORT,()=> {
-    console.log(`App up at port ${PORT}`);
-})
+// app.listen(PORT,()=> {
+//     console.log(`App up at port ${PORT}`);
+// })
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
