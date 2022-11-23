eaa65814dc368a1acdae90a6b4ed1f6c1e79ca74
mahersavara <kdlinh8811@gmail.com>
2022-11-24 02:57:48 +0700

    Updated Server

--- Js/app.js
+++ Js/app.js
@@ -15,12 +15,8 @@ router.get('/',function(req,res){
     //__dirname : It will resolve to your project folder.
 });
 
-// app.listen(PORT,()=> {
-//     console.log(`App up at port ${PORT}`);
-// })
-
-//add the router
 app.use('/', router);
+app.use(express.static('./Html'));
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
